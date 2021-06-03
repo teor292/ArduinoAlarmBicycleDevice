0af8dcb65ebf8c3e65cf158f37b0a7b8c26efcda
teor292 <teor292@gmail.com>
2021-06-03 20:06:43 +0300

    Update UBX messages

--- main/ubx_messages.h
+++ main/ubx_messages.h
@@ -1,6 +1,7 @@
 #pragma once
 
-#include <cstdint>
+#include <stdint.h>
+#include <Stream.h>
 
 struct UBX_Base
 {
@@ -9,25 +10,42 @@ struct UBX_Base
         id{id},
         length{length}
     {}
-    uint8_t header[2] = { 0xb5, 0x62};
     uint8_t clss;
     uint8_t id;
     uint16_t length;
 };
 
-struct UBX_Crc
-{
-    uint8_t ck_a{0};
-    uint8_t ck_b{0};
 
-    void Calculate(uint8_t* ptr, size_t size)
-    {
-        for (size_t i = 0; i < size; ++i)
-        {
-            ck_a = ck_a + ptr[i];
-            ck_b = ck_b + ck_a;
+template<typename T>
+class UBX_MESSAGE_
+{
+        uint8_t header_[2] = { 0xb5, 0x62};
+    public:
+
+        T message;
+
+        void Send(Stream& stream)
+        {     
+            crc_();
+            auto ptr = reinterpret_cast<uint8_t*>(this);
+            for (size_t i = 0; i < sizeof(*this); ++i)
+            {
+                stream.write(ptr[i]);
+            }
         }
-    }
+    private:
+        uint8_t ck_a_{0};
+        uint8_t ck_b_{0};
+
+        void crc_()
+        {
+            auto ptr = reinterpret_cast<uint8_t*>(&message); 
+            for (size_t i = 0; i < sizeof(message); ++i)
+            {
+                ck_a_ = ck_a_ + ptr[i];
+                ck_b_ = ck_b_ + ck_a_;
+            }
+        }
 };
 
 enum class POWER_SAVING_MODES : uint8_t
@@ -36,9 +54,9 @@ enum class POWER_SAVING_MODES : uint8_t
     PSMCT = 0x1
 };
 
-struct UBX_CFG_PM2 : UBX_Base
+struct UBX_CFG_PM2_BASE_ : UBX_Base
 {
-    UBX_CFG_PM2() 
+    UBX_CFG_PM2_BASE_() 
         : UBX_Base{0x06, 0x3b, 44}
     {}
 
@@ -65,7 +83,7 @@ struct UBX_CFG_PM2 : UBX_Base
             uint32_t doNotEnterOff : 1;
             uint32_t mode : 2;    
         };
-    }
+    };
     //if 0 -> receiver will never retry a fix and it will wait for external events
     uint32_t updatePeriod{0};
     //If set to 0, the receiver will never retry a startup
@@ -76,23 +94,22 @@ struct UBX_CFG_PM2 : UBX_Base
     //min time by default 1 min
     uint16_t minAcqTime{60};
     uint8_t reserved3[20]{};
-    UBX_Crc crc;
 
-    static UBX_CFG_PM2 GetDefaultPCMT()
+    static UBX_CFG_PM2_BASE_ GetDefaultPCMT()
     {
-        UBX_CFG_PM2 result;
+        UBX_CFG_PM2_BASE_ result;
         result.updatePeriod = 5000;
-        result.searchPeriod = 300 * 1000; //300 seconds
-        result.mode = POWER_SAVING_MODES::PSMCT;
+        result.searchPeriod = static_cast<uint32_t>(300) * 1000; //300 seconds
+        result.mode = static_cast<uint32_t>(POWER_SAVING_MODES::PSMCT);
         return result;
     };
 
-    static UBX_CFG_PM2 GetDefaultPCMOO()
+    static UBX_CFG_PM2_BASE_ GetDefaultPCMOO()
     {
-         UBX_CFG_PM2 result;
-         result.updatePeriod = 60 * 1000;
-         result.searchPeriod = 300 * 1000;
-         result.mode = POWER_SAVING_MODES::PSMOO;
+         UBX_CFG_PM2_BASE_ result;
+         result.updatePeriod = static_cast<uint32_t>(60) * 1000;
+         result.searchPeriod = static_cast<uint32_t>(300) * 1000;
+         result.mode = static_cast<uint32_t>(POWER_SAVING_MODES::PSMOO);
          return result;
     }
 };
@@ -101,49 +118,47 @@ enum class LP_MODE : uint8_t
 {
     CONTINOUS = 0,
     POWER_SAVING = 1
-}
+};
 
-struct UBX_CFG_RXM : UBX_Base
+struct UBX_CFG_RXM_BASE_ : UBX_Base
 {
-    UBX_CFG_RXM()
+    UBX_CFG_RXM_BASE_()
         : UBX_Base{0x06, 0x11, 2}
     {}
     uint8_t reserved1{0};
     LP_MODE lpMode{LP_MODE::CONTINOUS};
-    UBX_Crc crc;
 };
 
-struct UBX_CFG_REQUEST_RATE : UBX_Base
+struct UBX_CFG_REQUEST_RATE_BASE_ : UBX_Base
 {
-    UBX_CFG_RATE_Request()
+    UBX_CFG_REQUEST_RATE_BASE_()
         : UBX_Base{0x06, 0x08, 0}
-    UBX_Crc crc;
+    {}
 };
 
 enum class TIME_REF : uint16_t
 {
     UTC_TIME = 0,
     GPS_TIME = 1,
-    GLONASS_TIME = 2;
+    GLONASS_TIME = 2,
     BeiDou_TIME = 3,
     Galileo_TIME = 4
 };
 
-struct UBX_CFG_RATE : UBX_Base
+struct UBX_CFG_RATE_BASE_ : UBX_Base
 {
-    UBX_CFG_RATE()
+    UBX_CFG_RATE_BASE_()
         : UBX_Base{0x06, 0x08, 6}
     {}
 
     uint16_t measRate{1000};
     uint16_t navRate{1};
     TIME_REF timeRef{TIME_REF::UTC_TIME};
-    UBX_Crc crc;
 };
 
-struct UBX_RXM_PMREQ : UBX_Base
+struct UBX_RXM_PMREQ_BASE_ : UBX_Base
 {
-    UBX_RXM_PMREQ()
+    UBX_RXM_PMREQ_BASE_()
         : UBX_Base{0x02, 0x41, 8}
     {}
     //max 12 days, default 0 - not awake
@@ -158,5 +173,10 @@ struct UBX_RXM_PMREQ : UBX_Base
             uint8_t backup : 1;
         };
     };
-    UBX_Crc crc;
 };
+
+using UBX_CFG_PM2 = UBX_MESSAGE_<UBX_CFG_PM2_BASE_>;
+using UBX_CFG_RXM = UBX_MESSAGE_<UBX_CFG_RXM_BASE_>;
+using UBX_CFG_REQUEST_RATE = UBX_MESSAGE_<UBX_CFG_REQUEST_RATE_BASE_>;
+using UBX_CFG_RATE = UBX_MESSAGE_<UBX_CFG_RATE_BASE_>;
+using UBX_RXM_PMREQ = UBX_MESSAGE_<UBX_RXM_PMREQ_BASE_>;
