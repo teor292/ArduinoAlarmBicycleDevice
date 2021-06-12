56c4cabb2104c14c43f2a00848cce470b1073746
teor292 <teor292@gmail.com>
2021-06-12 19:59:40 +0300

    add first commands

--- main/GPSCommands.h
+++ main/GPSCommands.h
@@ -2,8 +2,20 @@
 
 enum class GPS_COMMANDS
 {
-    GET_GPS,
-    SET_GPS_REGIME,
-    GET_GPS_REGIME,
+    GET_GPS, //sms: get gps
+    SET_GPS_REGIME, //sms like: set gps regime wait
+    GET_GPS_REGIME, //sms like: get gps regime
+    SET_GPS_REGIME_WAIT, //sms: set gps wait <mode> [<mode_on_alarm>]
+    //available modes: all
+    GET_GPS_REGIME_WAIT, //sms: get gps wait
+
+    SET_GPS_REGIME_TRACK, //sms: set gps track <mode> [<mode_on_alarm>]
+    //available modes: CONTINOUS, PSMCT
+    GET_GPS_REGIME_TRACK, //sms: get gps track
+
+    SET_GPS_REGIME_TRAIL, //sms: set gps trail <mode> [<mode_on_alarm>]
+    //available modes: CONTINOUS, PSMCT, PSMOO
+    GET_GPS_REGIME_TRAIL //sms: get gps trail
+    
 
 };
